51679ac31286fcf38e1315e8f31e60254a2cb7f1
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-09-30 12:14:05 -0300

    ...

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 var res = document.querySelector('#res');
+
+function insert(num_opr) {
+    res.innerHTML += num_opr;
+}
+
+function clean() {
+    res.innerHTML = "";
+}
+
+function retry() {
+    res = document.querySelector('#res').innerHTML;
+    document.querySelector('#res').innerHTML = res.substring(
+    0,
+    res.length-1);
+}
