432ea490669394d4cc884294b40fff601ad84f3b
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-10-01 14:01:55 -0300

    AMÉM

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-var res = document.querySelector('#res');
-
-function insert(num_opr) {
-    res.innerHTML += num_opr;
+function inserir(num_opr) {
+    document.querySelector('#result').innerHTML += num_opr;
 }
 
-function clean() {
-    res.innerHTML = "";
+function apagar() {
+    let res = document.querySelector('#result').innerHTML;
+    document.querySelector('#result').innerHTML = res.substring(0, res.length - 1)
 }
 
-function retry() {
-    res = document.querySelector('#res').innerHTML;
-    document.querySelector('#res').innerHTML = res.substring(
-    0,
-    res.length-1);
+function limpar() {
+    document.querySelector('#result').innerHTML = "";
+}
+
+function calcular() {
+    let res = document.querySelector('#result').innerHTML;
+    if(res) {
+        document.querySelector('#result').innerHTML = eval(res);
+    } else {
+        document.querySelector('#result').innerHTML = "Nenhum comando";
+    }
 }
